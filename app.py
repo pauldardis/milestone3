b444e11994170960f9152f81c17fc6fcf1c04c3c
pauldardis <paul.dardis@bt.com>
2020-01-15 12:12:20 +0000

    inital commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import env
+
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
+if os.path.exists("env.py"):
+    import env
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get('DATABASE')
+if os.path.exists("env.py"):
+    app.config["MONGO_URI"] = env.mongo_uri
+else:
+    app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("recipe_list.html", 
+                            recipe_data=mongo.db.recipe_data.find())
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=(os.environ.get('PORT')),
+        debug=True)
