8e27ac59cc69dd4856607552e5e2af50b56c56d2
pauldardis <paul.dardis@bt.com>
2020-02-05 15:41:10 +0000

    changing app.py to fix Heroku issue'

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
 import os
-import env
-
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
-
-
 if os.path.exists("env.py"):
     import env
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get('DATABASE')
-if os.path.exists("env.py"):
-    app.config["MONGO_URI"] = env.mongo_uri
-    app.config['SECRET_KEY'] = env.SECRET_KEY
-else:
-    app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 mongo = PyMongo(app)
 
 
