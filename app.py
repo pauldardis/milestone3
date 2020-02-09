22facf12435e863fc4d956ef58a32bc21f630ef6
pauldardis <paul.dardis@bt.com>
2020-02-09 18:19:55 +0000

    changing app.py file to deploy on Heroku

--- app.py
+++ app.py
@@ -1,25 +1,37 @@
-import os
-import env
+# import os
+# import env
 
+# from flask import Flask, render_template, redirect, request, url_for, flash
+# from flask_pymongo import PyMongo
+# from bson.objectid import ObjectId
+
+
+
+# if os.path.exists("env.py"):
+#     import env
+# app = Flask(__name__)
+# app.config["MONGO_DBNAME"] = os.environ.get('DATABASE')
+# if os.path.exists("env.py"):
+#     app.config["MONGO_URI"] = env.mongo_uri
+#     app.config['SECRET_KEY'] = env.SECRET_KEY
+# else:
+#     app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+#     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+# mongo = PyMongo(app)
+
+import os
 from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
-
-
+ 
 if os.path.exists("env.py"):
-    import env
+   import env
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get('DATABASE')
-if os.path.exists("env.py"):
-    app.config["MONGO_URI"] = env.mongo_uri
-    app.config['SECRET_KEY'] = env.SECRET_KEY
-else:
-    app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
-mongo = PyMongo(app)
-
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+mongo = PyMongo(app
 
 @app.route('/')
 @app.route('/get_recipe')
