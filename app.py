2809e48cda79bd4c123eeaf8e5e3f184544cdd4c
pauldardis <paul.dardis@bt.com>
2020-02-12 18:36:09 +0000

    fixing error in app.py file and also env.py

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = os.environ.get('DATABASE')
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 mongo = PyMongo(app)
-mongo = PyMongo(app)
+
 
 
 @app.route('/')
